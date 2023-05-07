4549af33becd3eae464e2507412b141cb4443be8
MeirLejz <meirlejzerowicz@gmail.com>
2023-05-07 16:22:51 +0300

    add real mic button with style

--- src/components/InputMic/index.js
+++ src/components/InputMic/index.js
@@ -1,40 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { TouchableOpacity, Text, Image, StyleSheet } from "react-native";
+import { TouchableOpacity, Text, Image, StyleSheet, View, Dimensions } from "react-native";
 import { Audio } from "expo-av";
 import * as FileSystem from "expo-file-system";
 // import { Permissions } from "expo-permissions";
 import { PermissionsAndroid } from "react-native";
+
+const { width, height } = Dimensions.get('window');
+const buttonSize = Math.min(width, height) * 0.12;
 
 const Microphone = () => {
   const [recording, setRecording] = useState(null);
   const [isRecording, setIsRecording] = useState(false);
   const [sound, setSound] = useState(null);
   const [recordingPermission, setRecordingPermission] = useState(null);
-
-
-  // async function requestMicrophonePermission() {
-  //   try {
-  //     const result = await PermissionsAndroid.request(
-  //       PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-  //       {
-  //         title: "Microphone Permission",
-  //         message: "This app needs access to your microphone",
-  //         buttonNeutral: "Ask Me Later",
-  //         buttonNegative: "Cancel",
-  //         buttonPositive: "OK",
-  //       }
-  //     );
-
-  //     if (result === PermissionsAndroid.RESULTS.GRANTED) {
-
-  //       console.log("Microphone permission granted");
-  //     } else {
-  //       console.log("Microphone permission denied");
-  //     }
-  //   } catch (err) {
-  //     console.warn(err);
-  //   }
-  // }
 
   async function requestRecordAudioPermission() {
     try {
@@ -68,6 +46,7 @@ const Microphone = () => {
 
   const startRecording = async () => {
       try {
+        await requestRecordAudioPermission();
         const {recording} = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY);
         setRecording(recording);
         setIsRecording(true);
@@ -84,7 +63,6 @@ const Microphone = () => {
       setRecording(undefined);
       setIsRecording(false);
       await loadSound();
-      await playRecording();
     } catch (err) {
       console.error("Failed to stop recording", err);
     }
@@ -115,62 +93,39 @@ const Microphone = () => {
     }
   };
 
-  // const handleOnPress = async () => {
-  //   if (!recordingPermission) {
-  //     await requestRecordAudioPermission();
-  //   }
-  //   if (isRecording) {
-  //     console.log('Is recording', isRecording)
-  //     await stopRecording();
-  //   } else {
-  //     console.log('Is recording', isRecording)
-  //     await startRecording();
-  //   }
-  // };
-
   return (
     <>
-      {
-        !recordingPermission ? (
-          <TouchableOpacity onPress={requestRecordAudioPermission}>
-            <Image source={require("./icon.png")} style={styles.microphone} />
-          </TouchableOpacity> 
-        ) : (
-          isRecording ? (
-            <TouchableOpacity onPress={stopRecording}>
-              <Image source={require("./icon.png")} style={styles.microphone} />
-            </TouchableOpacity>
-          ) : (
-            <TouchableOpacity onPress={startRecording}>
-              <Image source={require("./icon.png")} style={styles.microphone} />
-            </TouchableOpacity>
-          )
-        )
-      }
-
-      {/* {recording ? (
-        <TouchableOpacity onPressIn={stopRecording} onPressOut>
-          <Text>Stop recording</Text>
-        </TouchableOpacity>
-      ) : (
-        <TouchableOpacity onPress={startRecording}>
-          <Text>Start recording</Text>
-        </TouchableOpacity>
-      )}
-      {Boolean(sound) && (
-        <TouchableOpacity onPress={playRecording}>
-          <Text>Play recording</Text>
-        </TouchableOpacity>
-      )} */}
+      {<TouchableOpacity 
+          onPressIn={recording ? undefined : startRecording}
+          onPressOut={recording ? stopRecording : undefined}
+          style={styles.microphoneButton}> 
+          <Image source={require("./icon.png")} style={styles.microphoneIcon} />
+        </TouchableOpacity>}
+      {Boolean(sound) && <TouchableOpacity onPress={playRecording}>
+              <Text>Play recording</Text>
+            </TouchableOpacity>}
     </>
   );
 };
 
 
 const styles = StyleSheet.create({
-  microphone: {
-    width: 100,
-    height: 100,
+  microphoneButton: {
+    width: buttonSize,
+    height: buttonSize,
+    borderRadius: buttonSize / 2,
+    backgroundColor: '#2196F3',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: height * 0.05,
+    alignSelf: 'center',
+    zIndex: 1,
+  },
+  microphoneIcon: {
+    width: buttonSize * 0.5,
+    height: buttonSize * 0.5,
+    resizeMode: 'contain',
   },
 });
 
